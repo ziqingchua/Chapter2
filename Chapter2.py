481dd26e76b9e2a476f197f4adfb26cb56616676
Zi Qing Chua <ziqingchua@swuser-129-8-193-18.swlan.csufresno.edu>
2022-02-22 14:23:12 -0800

    initial commit

---
+++ Chapter2.py
@@ -0,0 +1,20 @@
+
+
+print(3 + 2, 3 - 2, 3 * 2)
+print(8 /2, 8 ** 2, 2 * (3 +4))
+speed = 50 
+time_Elasped = 14 
+distance = speed * time_Elasped 
+print (distance) 
+a = 2
+b = 3
+print(abs(1-(4 *b)))
+print(int((a **b)+.8))
+print(round (a / b,3))
+num = 4
+
+total inches = 41
+feet = total_inches // 12
+inches = total_inches % 12
+print(feet, inches)
+
